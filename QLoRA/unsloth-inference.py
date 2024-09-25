9b71ee7972819144b43e98a8f2b4ff4a7c401677
nonbinary-soul <jchongoj@alumnos.unex.es>
2024-09-25 19:13:29 +0200

    Inference method improved

--- QLoRA/unsloth-inference.py
+++ QLoRA/unsloth-inference.py
@@ -1,58 +1,33 @@
 #!/home/lee/miniconda3/envs/unsloth_env/bin/python
-import torch
 import time 
+from llama_cpp import Llama
 
-from unsloth import FastLanguageModel
-from transformers import TextStreamer
+model_path = "./model/unsloth.Q8_0.gguf"
+ebo_model = Llama(model_path=model_path)
 
-def load_model_and_tokenizer(model_name, max_seq_length, dtype, load_in_4bit):
-    model, tokenizer = FastLanguageModel.from_pretrained(model_name, max_seq_length=max_seq_length, dtype=dtype, load_in_4bit=load_in_4bit)
+def generate_text_from_prompt(user_prompt, max_tokens = 100, temperature = 0.3, top_p = 0.1, echo = True, stop = ["<|end_of_text|>"]):
 
-    FastLanguageModel.for_inference(model)
-    
-    model.to("cuda")  # Mover el modelo a la GPU
-    return model, tokenizer
+   # Define the parameters
+   model_output = ebo_model(
+       user_prompt,
+       max_tokens=max_tokens,
+       temperature=temperature,
+       top_p=top_p,
+       echo=echo,
+       stop=stop,
+   )
 
-def generate_response(model, tokenizer, question):
-    llama_prompt = """You are an assistant
-<USER>
-{}
-
-<ASSISTANT>
-{}"""
-    
-    # Formatear el prompt para la generación
-    prompt = llama_prompt.format(question, "")
-    inputs = tokenizer([prompt], return_tensors="pt").to("cuda")
-
-    # Crear un streamer para capturar la salida generada
-    response_streamer = TextStreamer(tokenizer)
-
-    # Generar la respuesta
-    _ = model.generate(**inputs, streamer=response_streamer, max_new_tokens=128)
+   return model_output
 
 if __name__ == "__main__":
+    
     start_time = time.time()
     
-    model_name="llama-3-8b-bnb-4bit-qlora"  # Nombre del modelo guardado
-    max_seq_length=2048
-    dtype=None
-    load_in_4bit=True
-    
-    model, tokenizer = load_model_and_tokenizer(model_name, max_seq_length, dtype, load_in_4bit)
+    my_prompt = "hey, how are you?"
+    ebo_response = generate_text_from_prompt(my_prompt)
+    final_result = ebo_response["choices"][0]["text"].strip()
+    print("Model response:", final_result)
 
-    # Ejemplo de pregunta para obtener respuesta
-    question = "can you place the mug to the head of the table"
-    
-    start_response_time = time.time()
-   
-    print("Generated response: ")
-    generate_response(model, tokenizer, question)
-    
-    end_response_time = time.time()
-    total_response_time = end_response_time - start_response_time
-    print(f"Total response time: {total_response_time:.2f} second(s)")
-
-    end_time = time.time()
-    total_time = end_time - start_time
-    print(f"Execution time: {total_time:.2f} second(s)")
+    end_time = time.time()    
+    execution_time = end_time - start_time
+    print(f"Execution time: {execution_time:.2f} second(s)")
