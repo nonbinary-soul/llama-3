d1f6459c51de1b7d852a838076a64698dc643114
Lee <80453423+nonbinary-soul@users.noreply.github.com>
2024-10-01 11:38:10 +0200

    Update unsloth-rag.py

--- QLoRA/unsloth-rag.py
+++ QLoRA/unsloth-rag.py
@@ -37,8 +37,10 @@ db.add_texts(["harrison has one apple and two orange",
 template = """Answer the question based only on the following context: 
 {context}
 
----
-Answer the question based on the above context: {question}
+<USER>
+{question}
+
+<ASSISTANT>
 """
 prompt = PromptTemplate.from_template(template)
 
