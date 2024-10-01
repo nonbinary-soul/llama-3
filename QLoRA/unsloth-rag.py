5843e47756556369216ac4df73725768040fb702
Lee <80453423+nonbinary-soul@users.noreply.github.com>
2024-10-01 11:10:36 +0200

    Update unsloth-rag.py

--- QLoRA/unsloth-rag.py
+++ QLoRA/unsloth-rag.py
@@ -8,7 +8,6 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough
-from langchain_core.prompt_values import StringPromptValue
 
 # create the prompt
 template = """Answer the question based only on the following context: 
