dabb9a363738857928c58c0a85a62ea67cc59445
Lee <80453423+nonbinary-soul@users.noreply.github.com>
2024-10-01 10:58:17 +0200

    Update unsloth-rag.py

--- QLoRA/unsloth-rag.py
+++ QLoRA/unsloth-rag.py
@@ -1,15 +1,16 @@
 #!/home/lee/miniconda3/envs/unsloth_llamacpp/bin/python
 # this code has been copied from other source. 
 from huggingface_hub import hf_hub_download
-from llama_cpp import Llama
-from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import LlamaCpp
+from langchain_core.callbacks import CallbackManager, StreamingStdOutCallbackHandler
 from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from langchain.vectorstores.chroma import Chroma
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough
 
 model_path = "./model/unsloth.Q8_0.gguf"
-ebo_model = Llama(model_path=model_path)
+ebo_model = LlamaCpp(model_path=model_path, n_gpu_layers=-1, n_batch=512, callback_manager=callback_manager, verbose=True)
 
 # create the embeddings
 model_name = "mixedbread-ai/mxbai-embed-large-v1"
@@ -40,9 +41,11 @@ Answer the question based on the above context: {question}
 
 prompt = PromptTemplate.from_template(template)
 
+# Callbacks support token-wise streaming
+callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+
 # create the chain
 output_parser = StrOutputParser()
-
 
 def format_docs(docs):
 
